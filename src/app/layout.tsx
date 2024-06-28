31bfd376db46407a84aa19bff90436bec091accd
CydoEntis <cydoentis@gmail.com>
2024-06-28 19:27:29 -0400

    Added theme switcher, fixed main layout squishing when theme toggle was open.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import Link from "next/link";
 import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import { ThemeToggle } from "@/components/theme/theme-toggle";
 import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
@@ -24,30 +26,44 @@ export default function RootLayout({
 		<html lang="en">
 			<body
 				className={cn(
-					"min-h-screen bg-background font-sans antialiased max-w-screen-2xl bg-stone-900 mx-auto",
+					"min-h-screen bg-background font-sans antialiased mx-auto w-full",
 					fontSans.variable,
 				)}
 			>
-				{/* Todo Turn this Navigation into a reusable component. */}
-				<header className="w-full h-24 fixed z-50 py-4 max-w-screen-2xl">
-					<nav className=" bg-neutral-800 h-full mx-auto rounded-full shadow-lg flex items-center px-12 justify-between">
-						<div>
-							<h3 className="text-white text-2xl font-bold">
-								Side
-								<span className="text-blue-500">Hustle</span>
-							</h3>
-						</div>
-						<ul className="flex gap-4 items-center">
-							<li className="font-semibold text-white hover:text-blue-500 transition-colors ease-in-out duration-300">
-								<Link href="/login">Login</Link>
-							</li>
-							<li className="font-semibold text-white hover:text-blue-500 transition-colors ease-in-out duration-300">
-								<Link href="/register">Register</Link>
-							</li>
-						</ul>
-					</nav>
-				</header>
-				{children}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<main className="max-w-screen-2xl  mx-auto">
+						{/* Todo Turn this Navigation into a reusable component. */}
+						<header className="w-full h-24 fixed z-50 py-4 max-w-screen-2xl">
+							<nav className=" bg-neutral-800 h-full mx-auto rounded-full shadow-lg flex items-center px-12 justify-between">
+								<div>
+									<h3 className="text-white text-2xl font-bold">
+										Side
+										<span className="text-blue-500">Hustle</span>
+									</h3>
+								</div>
+								<ul className="flex gap-4 items-center">
+									<li className="font-semibold text-white hover:text-blue-500 transition-colors ease-in-out duration-300">
+										<Link href="/login">Login</Link>
+									</li>
+									<li className="font-semibold text-white hover:text-blue-500 transition-colors ease-in-out duration-300">
+										<Link href="/register">Register</Link>
+									</li>
+									<li>
+										<div className="">
+											<ThemeToggle />
+										</div>
+									</li>
+								</ul>
+							</nav>
+						</header>
+						{children}
+					</main>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
